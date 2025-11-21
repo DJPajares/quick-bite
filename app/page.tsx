ab4963c3294cf4b0200e1092f6a3c94aba2947e1
Darwin Jason Pajares <xsgdp001@singlife.com>
2025-11-21 10:25:26 +0800

    chore: prepare script

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,18 @@ import { Label } from '@/components/ui/label';
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
-  const t = useTranslations('Home');
+  const t = useTranslations();
 
   return (
     <main className="flex min-h-screen items-center justify-center bg-background font-sans">
       <div className="flex flex-col items-center gap-6">
-        <Label>{t('title')}</Label>
+        <Label>{t('Home.title')}</Label>
 
         <Button
           variant="outline"
           size="lg"
         >
-          Continue
+          {t('Button.continue')}
         </Button>
       </div>
     </main>
